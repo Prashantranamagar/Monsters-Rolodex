7735c547ff044223b6e06234de7bccfb92826d8c
Prashant Rana Magar <ranamagar.prashant@gamil.com>
2022-07-01 15:12:20 +0545

    created monsters-rolodex app class components, states

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      name: 'Person',
+    };
+  }
+  render()  {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p>HI {this.state.name}</p>
+            <button 
+            onClick={() => {
+              this.setState({ name: 'Rana'});
+            }}
+            >
+              Change Name
+            </button>
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
